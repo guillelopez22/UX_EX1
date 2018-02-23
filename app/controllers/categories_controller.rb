a57dea13f39c87606d5b2c96bcc3e3cdf730bb51
guillelopez22 <guillelopez_13@hotmail.com>
2018-02-22 21:26:58 -0800

    updates

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,19 +2,38 @@ class CategoriesController < ApplicationController
   def index
   	@category = Category.all.order('created_at DESC')
   end
+
+
   def new 
     @category = Category.new
   end
 
   def create
   	@category = Category.new(post_params)
-  	@category.save
-
-  	redirect_to @category
+    if @category.save
+      reddirect_to @category
+    else 
+      render 'new'
+    end
   end
+
   def show
   	@category = Category.find(params[:id])
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(params[:category].permit(:name))
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
   	def post_params
   		params.require(:category).permit(:name)
